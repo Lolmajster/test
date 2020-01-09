4c4cd90d4a98fb6256b47bf1e86421572a2e097d
Lolmajster <lolorav@gmail.com>
2020-01-09 13:35:44 +0100

    skonczone okno modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,28 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+// import Modal from './modules/Modal';
 
-let stickyHeader = new StickyHeader;
-let mobileMenu = new MobileMenu;
+// new Modal;
+new StickyHeader;
+new MobileMenu;
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+let modal;
 
-
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", event => {
+        event.preventDefault();
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openModal(), 20)
+            }).catch(() => console.log("ERROR!"))
+        } else {
+            modal.openModal();
+        }
+    })
+});
 
 if(module.hot) {
     module.hot.accept();
