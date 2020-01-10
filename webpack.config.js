7c5382d0fba24e770df937cbe40a236fb4214bcc
Lolmajster <lolorav@gmail.com>
2020-01-10 11:16:42 +0100

    przygotowany build na gita

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse = require('fs-extra');
+
 const postCSSPlugins = [
     require('postcss-import'),
     require('postcss-mixins'),
@@ -8,13 +14,45 @@ const postCSSPlugins = [
     require('autoprefixer')
 ];
 
-module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output: {
+class RunAfterComplie {
+    apply(compiler) {
+        compiler.hooks.done.tap('Copy images', function() {
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })
+    }
+}
+
+let cssConfig = {
+    test: /\.css$/i,
+    use: ['css-loader', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+}
+
+let pages = fse.readdirSync('./app').filter(function(file) {
+    return file.endsWith('.html')
+}).map(function(page) {
+    return new HtmlWebpackPlugin({
+        filename: page,
+        template: `./app/${page}`
+    })
+})
+
+let config = {
+    entry: './app/assets/scripts/App.js', // wspolne dla dev i build
+    plugins: pages,
+    module: {
+        rules: [
+            cssConfig
+        ]
+    }
+};
+
+if (currentTask == 'dev') {
+    cssConfig.use.unshift('style-loader'),
+    config.output = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
-    devServer: {
+    config.devServer = {
         before: function(app, server) {
             server._watch('./app/**/*.html')
         },
@@ -23,14 +61,66 @@ module.exports = {
         port: 3000,
         host: '0.0.0.0'
     },
-    mode: 'development',
-    // watch: true, // nie jest potrzebne bo przeszedłem na webpack-dev-server
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
-            }
-        ]
-    }
+    config.mode = 'development'
 }
+
+if (currentTask == 'build') {
+    config.module.rules.push({
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    })
+
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader),
+    postCSSPlugins.push(require('cssnano')),
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    },
+    config.mode = 'production',
+    config.optimization = {
+        splitChunks: {chunks: 'all'}
+    },
+    config.plugins.push(
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+        new RunAfterComplie()
+    )
+};
+
+
+
+let deleteMeLater = {
+    // entry: './app/assets/scripts/App.js',
+    // output: {
+    //     filename: 'bundled.js',
+    //     path: path.resolve(__dirname, 'app')
+    // },
+    // devServer: {
+    //     before: function(app, server) {
+    //         server._watch('./app/**/*.html')
+    //     },
+    //     contentBase: path.join(__dirname, 'app'),
+    //     hot: true,
+    //     port: 3000,
+    //     host: '0.0.0.0'
+    // },
+    // mode: 'development',
+    // watch: true, // nie jest potrzebne bo przeszedłem na webpack-dev-server
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.css$/i,
+    //             use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+    //         }
+    //     ]
+    // }
+}
+
+module.exports = config;
