c563cb5eefef9a7bda0617623c4544ab3d0f4be7
Lolmajster <lolorav@gmail.com>
2020-01-10 14:15:17 +0100

    netlify ;) test z postmanem i z formem

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,12 @@ import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import ClientArea from './modules/ClientArea';
+
 // import Modal from './modules/Modal';
 
 // new Modal;
+new ClientArea;
 new StickyHeader;
 new MobileMenu;
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
