3282f0e25ab0f6fd67c9b70cc3ddeeb7d55c893a
Lolmajster <lolorav@gmail.com>
2020-01-10 13:33:37 +0100

    netlify ;) test z postmanem

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,7 +1,28 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "To jest sekretna strefa!"
-    })
+    const secretContent = `
+        <h3>Witaj w sekretnej strefie!</h3>
+        <p>taki sobie <strong>tekst</strong></p>
+    `
+    let body;
+
+    if(event.body) {
+        body = JSON.parse(event.body);
+    } else {
+        body = {};
+    }
+
+    if(body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
+    
+    
+    
 
 }
